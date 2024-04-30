3c71ae88fa4c7406ce2e24c45d895d55d156d709
Bikram <ambreeshpatel98@gmail.com>
2024-05-01 00:51:41 +0530

    created serializers

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from core.models import Vender
+from core.models import Vender, Purchase_Order, Historical_Performance
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.ModelSerializer):
+class VanderSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = Vender
         fields = '__all__'
+        
+        
+class Purchase_OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Purchase_Order
+        
+        fields = '__all__'
+        
+        
+class Historical_PerformanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Historical_Performance
+        fields = '__all__' 
