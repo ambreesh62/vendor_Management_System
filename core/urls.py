76fd96508658fce8d8d288ff1db3d1375bafc56d
Bikram <ambreeshpatel98@gmail.com>
2024-05-01 15:30:24 +0530

    Created Vender APIView

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from core.views import VenderAPIView
 urlpatterns = [
-    path('vender', VenderAPIView.as_view())
+    path('vender', VenderAPIView.as_view()),
+    path('vender/<int:pk>', VenderAPIView.as_view())
 ]
 
--- core/views.py
+++ core/views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 
 
 
-# Create your views here.
+# Vender APIViews here.
 
 class VenderAPIView(APIView):
     def post(self, request):
@@ -17,10 +17,41 @@ class VenderAPIView(APIView):
             print(serilalizer.errors)
             return Response({'errors' : serilalizer.errors, 'massage' : 'somthing went worng'}, status=status.HTTP_400_BAD_REQUEST)
         serilalizer.save()
-        return Response({'playload' : serilalizer.data, 'massage' : 'data saved'}, status=status.HTTP_201_CREATED)
+        return Response({'playload' : serilalizer.data, 'massage' : 'Your data saved'}, status=status.HTTP_201_CREATED)
     
     
-    def get(self, request):
+    def get(self, request, pk=None):
+        
+        if pk:
+            vender_objs = Vender.objects.get(id=pk)
+            serializer = VanderSerializer(vender_objs)
+            return Response({'massage': 'success', 'data': serializer.data}, status=status.HTTP_200_OK)
+        
         vender_objs = Vender.objects.all()
         serializer = VanderSerializer(vender_objs, many=True)
         return Response({'playload' : serializer.data}, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk=None):
+        
+        try:
+            
+            vender_objs = Vender.objects.get(pk=pk)
+            seralizer = VanderSerializer(vender_objs, data=request.data)
+            
+            if not seralizer.is_valid():
+                print(seralizer.errors)
+                return Response({'errors' : seralizer.errors, 'massage' : 'somthing went worng'}, status=status.HTTP_400_BAD_REQUEST)
+            seralizer.save()
+            return Response({'playload' : seralizer.data,}, status=status.HTTP_202_ACCEPTED)
+
+        except Vender.DoesNotExist:
+            return Response({'massage' : 'DoesNotExist'}, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request, pk=None):
+        
+        try:
+            vender_objs = Vender.objects.get(pk=pk)
+            vender_objs.delete()
+            return Response({'massage' : 'Data has been deleted'})
+        except Vender.DoesNotExist:
+            return Response({'massage' : 'DoesNotExist'}, status=status.HTTP_400_BAD_REQUEST)    
